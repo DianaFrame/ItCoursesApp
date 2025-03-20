f8f5759cf1dc3853bdc6841d5eef7ac2d708dd51
diana <diana.frame17@vk.com>
2025-03-20 21:05:16 +0300

    написала интерфейс CoursesApi для работы с мок JSON API

--- data/src/main/java/com/example/data/api/CoursesApi.kt
+++ data/src/main/java/com/example/data/api/CoursesApi.kt
@@ -1,4 +1,12 @@
 package com.example.data.api
 
-class CoursesApi {
+import com.example.data.models.Courses
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface CoursesApi {
+
+    @GET("courses.json")
+    fun getAllCourses(): Response<Courses>
+
 }
